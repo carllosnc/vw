6be561f08915098da6f32d3c6ec61e95544d7baf
Carlos Costa <carllos.nc@gmail.com>
2023-11-20 15:14:12 -0300

    Add properties and documentation to VWRow class

--- lib/vw_row.dart
+++ lib/vw_row.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 
 class VWRow extends StatelessWidget {
+  ///[children] A list of widgets to display in the row. Default is an empty list.
   final List<Widget> children;
+
+  ///[gap] The gap between the widgets. Default is 0.
   final double gap;
+
+  ///[padding] The padding around the row. Default is EdgeInsets.all(0).
   final EdgeInsetsGeometry padding;
+
+  ///[margin] The margin around the row. Default is EdgeInsets.all(0).
   final EdgeInsetsGeometry margin;
+
+  ///[expandItems] A boolean value that determines whether the items in the row should expand to fill the available space. Default is false.
+  ///If this is true, [scrollable] must be false.
   final bool expandItems;
+
+  ///[scrollable] A boolean value that determines whether the row is scrollable. Default is false.
+  ///If this is true, [expandItems] must be false.
   final bool scrollable;
+
+  ///[reverseScroll] A boolean value that determines whether the scroll direction is reversed. Default is false.
   final bool reverseScroll;
+
+  ///[physics] The physics type that determines how the row scrolls. Default is ClampingScrollPhysics.
   final ScrollPhysics physics;
+
+  ///[horizontalAlignment] The horizontal alignment of the widgets in the row. Default is MainAxisAlignment.start.
   final MainAxisAlignment horizontalAlignment;
+
+  ///[verticalAlignment] The vertical alignment of the widgets in the row. Default is CrossAxisAlignment.start.
   final CrossAxisAlignment verticalAlignment;
+
+  ///[controller] A ScrollController that controls the position of the scroll. Default is null.
   final ScrollController? controller;
 
   const VWRow({
