03b8d2e8bfea4933893fa36720e1b157f1cbbdb3
Carlos Costa <carllos.nc@gmail.com>
2023-11-25 13:03:35 -0300

    Add VWPageExample to routes

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './examples/vw_row_example.dart';
 import './examples/vw_column_example.dart';
 import './examples/vw_button_example.dart';
+import './examples/vw_page_example.dart';
 
 import 'home.dart';
 
@@ -10,16 +11,35 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const pageTransition = PageTransitionsTheme(
+      builders: {
+        TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+        TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+      },
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
       home: const Home(),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+        colorSchemeSeed: Colors.red,
+        //set bottom sheet animation
+        pageTransitionsTheme: pageTransition,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+        colorSchemeSeed: Colors.red,
+        //set bottom sheet animation
+        pageTransitionsTheme: pageTransition,
+      ),
       routes: {
         '/vw_row': (context) => const VWRowExamplePage(),
         '/vw_column': (context) => const VWColumnExamplePage(),
         '/vw_button': (context) => const VWButtonExample(),
+        '/vw_page': (context) => const VWPageExample(),
       },
     );
   }
