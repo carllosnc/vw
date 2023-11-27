b831619fca087e882013a1e6e2512a195296e5c1
Carlos Costa <carllos.nc@gmail.com>
2023-11-27 10:19:48 -0300

    Add example using bottomNavigationBar

--- example/lib/examples/vw_button_example.dart
+++ example/lib/examples/vw_button_example.dart
@@ -15,206 +15,216 @@ class _VWButtonExampleState extends State<VWButtonExample> {
       appBar: AppBar(
         title: const Text('VW Buttons'),
       ),
-      body: VWColumn(
-        padding: const EdgeInsets.all(10),
-        gap: 10,
+      bottomNavigationBar: const VWRow(
         children: [
-          const VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Default button",
-              ),
-            ],
-          ),
-          const VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Red",
-                color: Colors.red,
-              ),
-              VWButton(
-                label: "Green",
-                color: Colors.green,
-              ),
-              VWButton(
-                label: "Blue",
-                color: Colors.blue,
-              ),
-            ],
-          ),
-          const VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Full width",
-                color: Colors.red,
-                fullWidth: true,
-              ),
-            ],
-          ),
-          VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Light button",
-                color: Colors.grey.shade300,
-                splashColor: Colors.black,
-                fullWidth: true,
-              ),
-            ],
-          ),
-          //dark button
-          VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Dark button",
-                color: Colors.grey.shade800,
-                fullWidth: true,
-              ),
-            ],
-          ),
-          const VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Full width",
-                fullWidth: true,
-                labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w900,
-                  letterSpacing: 2,
-                ),
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.red,
-                    Colors.yellow,
-                  ],
-                ),
-              ),
-            ],
-          ),
-          const VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Loading",
-                fullWidth: true,
-                isLoading: true,
-              ),
-              VWButton(
-                label: "",
-                fullWidth: true,
-                isLoading: true,
-              ),
-            ],
-          ),
-          const VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Left icon",
-                fullWidth: true,
-                loadingLabel: "Loading",
-                leftIcon: Icons.favorite,
-              ),
-              VWButton(
-                label: "Right icon",
-                fullWidth: true,
-                loadingLabel: "Loading",
-                rightIcon: Icons.favorite,
-              ),
-            ],
-          ),
-          const VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Right icon",
-                fullWidth: true,
-                loadingLabel: "Loading",
-                rightIcon: Icons.favorite,
-                leftIcon: Icons.favorite,
-              ),
-            ],
-          ),
-          VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Gradient",
-                fullWidth: true,
-                loadingLabel: "Outlined button",
-                color: Colors.white,
-                splashColor: Colors.black,
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1,
-                ),
-              ),
-            ],
-          ),
-
-          const VWRow(
-            gap: 10,
-            children: [
-              VWButton(
-                label: "Gradient",
-                fullWidth: true,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              ),
-            ],
-          ),
-
-          VWRow(
-            gap: 10,
-            verticalAlignment: CrossAxisAlignment.center,
-            children: [
-              VWButton(
-                onTap: () => debugPrint("Button tapped!"),
-                label: "On tap",
-                fullWidth: true,
-              ),
-            ],
-          ),
-
-          VWRow(
-            gap: 10,
-            verticalAlignment: CrossAxisAlignment.center,
-            children: [
-              VWButton(
-                onDoubleTap: () => debugPrint("Button double tapped!"),
-                label: "Align left",
-                fullWidth: true,
-                horizontalAlignment: MainAxisAlignment.start,
-              ),
-              VWButton(
-                onDoubleTap: () => debugPrint("Button double tapped!"),
-                label: "Align left",
-                fullWidth: true,
-                horizontalAlignment: MainAxisAlignment.end,
-              ),
-            ],
-          ),
-
-          VWRow(
-            gap: 10,
-            verticalAlignment: CrossAxisAlignment.center,
-            children: [
-              VWButton(
-                onDoubleTap: () => debugPrint("Button double tapped!"),
-                label: "Align left",
-                fullWidth: true,
-                leftIcon: Icons.favorite,
-                horizontalAlignment: MainAxisAlignment.spaceBetween,
-              ),
-            ],
+          VWButton(
+            label: "Button",
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: VWColumn(
+          padding: const EdgeInsets.all(10),
+          gap: 10,
+          children: [
+            const VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Default button",
+                ),
+              ],
+            ),
+
+            const VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Red",
+                  color: Colors.red,
+                ),
+                VWButton(
+                  label: "Green",
+                  color: Colors.green,
+                ),
+                VWButton(
+                  label: "Blue",
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+            const VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Full width",
+                  color: Colors.red,
+                  fullWidth: true,
+                ),
+              ],
+            ),
+            VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Light button",
+                  color: Colors.grey.shade300,
+                  splashColor: Colors.black,
+                  fullWidth: true,
+                ),
+              ],
+            ),
+            //dark button
+            VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Dark button",
+                  color: Colors.grey.shade800,
+                  fullWidth: true,
+                ),
+              ],
+            ),
+            const VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Full width",
+                  fullWidth: true,
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 2,
+                  ),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.red,
+                      Colors.yellow,
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Loading",
+                  fullWidth: true,
+                  isLoading: true,
+                ),
+                VWButton(
+                  label: "",
+                  fullWidth: true,
+                  isLoading: true,
+                ),
+              ],
+            ),
+            const VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Left icon",
+                  fullWidth: true,
+                  loadingLabel: "Loading",
+                  leftIcon: Icons.favorite,
+                ),
+                VWButton(
+                  label: "Right icon",
+                  fullWidth: true,
+                  loadingLabel: "Loading",
+                  rightIcon: Icons.favorite,
+                ),
+              ],
+            ),
+            const VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Right icon",
+                  fullWidth: true,
+                  loadingLabel: "Loading",
+                  rightIcon: Icons.favorite,
+                  leftIcon: Icons.favorite,
+                ),
+              ],
+            ),
+            VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Gradient",
+                  fullWidth: true,
+                  loadingLabel: "Outlined button",
+                  color: Colors.white,
+                  splashColor: Colors.black,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                  ),
+                ),
+              ],
+            ),
+
+            const VWRow(
+              gap: 10,
+              children: [
+                VWButton(
+                  label: "Gradient",
+                  fullWidth: true,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+              ],
+            ),
+
+            VWRow(
+              gap: 10,
+              verticalAlignment: CrossAxisAlignment.center,
+              children: [
+                VWButton(
+                  onTap: () => debugPrint("Button tapped!"),
+                  label: "On tap",
+                  fullWidth: true,
+                ),
+              ],
+            ),
+
+            VWRow(
+              gap: 10,
+              verticalAlignment: CrossAxisAlignment.center,
+              children: [
+                VWButton(
+                  onDoubleTap: () => debugPrint("Button double tapped!"),
+                  label: "Align left",
+                  fullWidth: true,
+                  horizontalAlignment: MainAxisAlignment.start,
+                ),
+                VWButton(
+                  onDoubleTap: () => debugPrint("Button double tapped!"),
+                  label: "Align left",
+                  fullWidth: true,
+                  horizontalAlignment: MainAxisAlignment.end,
+                ),
+              ],
+            ),
+
+            VWRow(
+              gap: 10,
+              verticalAlignment: CrossAxisAlignment.center,
+              children: [
+                VWButton(
+                  onDoubleTap: () => debugPrint("Button double tapped!"),
+                  label: "Align left",
+                  fullWidth: true,
+                  leftIcon: Icons.favorite,
+                  horizontalAlignment: MainAxisAlignment.spaceBetween,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
