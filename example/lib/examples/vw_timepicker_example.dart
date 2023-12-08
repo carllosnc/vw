1e3cf1e7ab4cdda3c271d82597bcbf5a2ab6592e
Carlos Costa <carllos.nc@gmail.com>
2023-12-08 16:07:00 -0300

    Add VWTimepickerExample widget

---
+++ example/lib/examples/vw_timepicker_example.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:vw/vw.dart';
+
+class VWTimepickerExample extends StatelessWidget {
+  const VWTimepickerExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('VW Components'),
+      ),
+      body: SingleChildScrollView(
+        child: VWColumn(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          children: [
+            VWTimePicker(
+              cancelText: "Cancelar",
+              confirmText: "Confirmar",
+              errorInvalidText: "Formato de hora inválido",
+              hourLabelText: "Hora",
+              minuteLabelText: "Minuto",
+              helpText: "Selecione a hora",
+              onChanged: (TimeOfDay value) {
+                debugPrint("Time selected: $value");
+              },
+              decoration: const InputDecoration(
+                labelText: 'Select time',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
