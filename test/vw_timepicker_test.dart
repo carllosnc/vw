9d05dafca901a7892201f3062cf06ec3fa82ec53
Carlos Costa <carllos.nc@gmail.com>
2023-12-08 16:07:17 -0300

    Add VWTimePicker initial render test

---
+++ test/vw_timepicker_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:vw/vw.dart';
+
+void main() {
+  testWidgets('Check timepicker initial render', (tester) async {
+    TimeOfDay? selectedTime;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: VWTimePicker(
+            onChanged: (time) {
+              selectedTime = time;
+            },
+          ),
+        ),
+      ),
+    );
+
+    expect(find.byKey(const Key('vw_timepicker_input')), findsOneWidget);
+
+    await tester.tap(find.byKey(const Key('vw_timepicker_input')));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Select time'), findsOneWidget);
+    expect(find.text('Cancel'), findsOneWidget);
+    expect(find.text('Ok'), findsOneWidget);
+    expect(find.text('Hour'), findsOneWidget);
+    expect(find.text('Minute'), findsOneWidget);
+
+    await tester.tap(find.text('Ok'));
+    await tester.pumpAndSettle();
+    expect(selectedTime, isNotNull);
+
+    final now = DateTime.now();
+
+    expect(selectedTime!.hour, now.hour);
+  });
+}
