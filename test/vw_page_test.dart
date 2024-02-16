6bce8a02426945748c047d7b1dad13e9c00f02a9
Carlos Costa <carllos.nc@gmail.com>
2024-02-15 21:45:06 -0300

    adding test case for multiple pages

--- test/vw_page_test.dart
+++ test/vw_page_test.dart
@@ -4,7 +4,7 @@ import 'package:vw/vw.dart';
 
 void main() {
   testWidgets(
-    'vw page ...',
+    'Check initial render',
     (tester) async {
       await tester.pumpWidget(
         MaterialApp(
@@ -45,6 +45,89 @@ void main() {
       expect(find.text('VW Page header'), findsOneWidget);
       expect(find.text('VW Page body'), findsOneWidget);
       expect(find.text('VW Page bottom bar'), findsOneWidget);
+    },
+  );
+
+  //check page inside page
+  testWidgets(
+    'Check page inside page',
+    (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Builder(builder: (context) {
+              return FilledButton(
+                child: const Text('Open first page'),
+                onPressed: () {
+                  VWPage.open(
+                    level: 1,
+                    context: context,
+                    appBar: AppBar(
+                      title: const Text('First page'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('first back'),
+                        )
+                      ],
+                    ),
+                    body: Center(
+                      child: FilledButton(
+                        child: const Text('open second page'),
+                        onPressed: () {
+                          VWPage.open(
+                            level: 2,
+                            context: context,
+                            appBar: AppBar(
+                              title: const Text('Second page'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('second back'),
+                                )
+                              ],
+                            ),
+                            body: const Center(
+                              child: Text('End'),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  );
+                },
+              );
+            }),
+          ),
+        ),
+      );
+
+      await tester.tap(find.text('Open first page'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('First page'), findsOneWidget);
+      expect(find.text('open second page'), findsOneWidget);
+
+      await tester.tap(find.text('open second page'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Second page'), findsOneWidget);
+      expect(find.text('End'), findsOneWidget);
+
+      await tester.tap(find.text('second back'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('First page'), findsOneWidget);
+      expect(find.text('open second page'), findsOneWidget);
+
+      await tester.tap(find.text('first back'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Open first page'), findsOneWidget);
     },
   );
 }
