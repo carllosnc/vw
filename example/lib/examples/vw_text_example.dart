cca6a9e344127c40bb949f27ac48b966d82e8398
Carlos Costa <carllos.nc@gmail.com>
2024-09-24 14:24:38 -0300

    Set title

--- example/lib/examples/vw_text_example.dart
+++ example/lib/examples/vw_text_example.dart
@@ -10,7 +10,7 @@ class VWTextExample extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Title'),
+        title: const Text('VWText'),
       ),
       body: SingleChildScrollView(
         child: VWColumn(
